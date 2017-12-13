6e889a8624a3fae157c3dfa64f688e3b567a0507
J0ssy <j.g.basterrica@gmail.com>
2017-12-13 13:11:44 -0300

    mouseover

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,36 +5,67 @@ $(document).ready(function(){
     $('.second-section').show();
   }, 2000);
 
+  $('.picture-1').mouseover(function () {
+    $('.picture-content-1').show();
+  }).mouseout(function () {
+    $('.picture-content-1').hide();
+  });
+
+  $('.picture-2').mouseover(function () {
+    $('.picture-content-2').show();
+  }).mouseout(function () {
+    $('.picture-content-2').hide();
+  });
+
+  $('.picture-3').mouseover(function () {
+    $('.picture-content-3').show();
+  }).mouseleave(function () {
+    $('.picture-content-3').hide();
+  });
+
+  $('.picture-4').mouseover(function () {
+    $('.picture-content-4').show();
+  }).mouseout(function () {
+    $('.picture-content-4').hide();
+  });
+
+  $('.picture-5').mouseover(function () {
+    $('.picture-content-5').show();
+  }).mouseout(function () {
+    $('.picture-content-5').hide();
+  });
+
+
   $('.dropdown-menu li:first').click(chinese);
   $('.dropdown-menu li:nth-child(2)').click(arabian);
   $('.dropdown-menu li:last-child').click(japanese);
   $('.first-section h1').click(homeresto);
 
   function chinese( ){
-    $('.home-resto').fadeOut(500);
-    $('.arabian').fadeOut(500);
-    $('.japanese').fadeOut(500);
+    $('.home-resto').fadeOut();
+    $('.arabian').fadeOut();
+    $('.japanese').fadeOut();
     $('.chinese').show();
   }
 
   function arabian() {
-    $('.home-resto').fadeOut(500);
-    $('.chinese').fadeOut(500);
-    $('.japanese').fadeOut(500);
+    $('.home-resto').fadeOut();
+    $('.chinese').fadeOut();
+    $('.japanese').fadeOut();
     $('.arabian').show();
   }
 
   function japanese() {
-    $('.home-resto').fadeOut(500);
-    $('.chinese').fadeOut(500);
-    $('.arabian').fadeOut(500);
+    $('.home-resto').fadeOut();
+    $('.chinese').fadeOut();
+    $('.arabian').fadeOut();
     $('.japanese').show();
   }
 
   function homeresto() {
-    $('.japanese').fadeOut(500);
-    $('.chinese').fadeOut(500);
-    $('.arabian').fadeOut(500);
+    $('.japanese').fadeOut();
+    $('.chinese').fadeOut();
+    $('.arabian').fadeOut();
     $('.home-resto').show();
   }
   
