9f079fa4878db7bb02d0ccd35e33002913efa813
J0ssy <j.g.basterrica@gmail.com>
2017-12-13 09:35:28 -0300

    categorías listas

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,38 @@ $(document).ready(function(){
     $('.second-section').show();
   }, 2000);
 
+  $('.dropdown-menu li:first').click(chinese);
+  $('.dropdown-menu li:nth-child(2)').click(arabian);
+  $('.dropdown-menu li:last-child').click(japanese);
+  $('.first-section h1').click(homeresto);
 
+  function chinese( ){
+    $('.home-resto').fadeOut(500);
+    $('.arabian').fadeOut(500);
+    $('.japanese').fadeOut(500);
+    $('.chinese').show();
+  }
 
+  function arabian() {
+    $('.home-resto').fadeOut(500);
+    $('.chinese').fadeOut(500);
+    $('.japanese').fadeOut(500);
+    $('.arabian').show();
+  }
 
+  function japanese() {
+    $('.home-resto').fadeOut(500);
+    $('.chinese').fadeOut(500);
+    $('.arabian').fadeOut(500);
+    $('.japanese').show();
+  }
 
+  function homeresto() {
+    $('.japanese').fadeOut(500);
+    $('.chinese').fadeOut(500);
+    $('.arabian').fadeOut(500);
+    $('.home-resto').show();
+  }
+  
 
 });
